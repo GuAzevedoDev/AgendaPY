ae5332fae5b0fef6e39dfb16da5b57f3d1fe6637
Gustavo Azevedo <gustavoazevedo.code@gmail.com>
2026-07-08 17:17:57 -0300

    flask sqalchemy

--- models/base.py
+++ models/base.py
@@ -1,1 +1,6 @@
-class
+from datetime import datetime
+
+class Base(db.Model):
+  __abstract__ = True
+  id = db.Column(db.Integer,primary_key = True)
+  data_de_criação = db.Column(db.Datetime, default = datetime.now, nullable = False)
